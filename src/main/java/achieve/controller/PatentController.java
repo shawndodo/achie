0da1635428a0201f071783714d5b95c9cbaf7caf
zhuxiaoyu <dodocat6666@hotmail.com>
2018-06-15 00:16:14 +0800

    add .

--- src/main/java/achieve/controller/PatentController.java
+++ src/main/java/achieve/controller/PatentController.java
@@ -43,33 +43,44 @@ public class PatentController extends BaseController {
     private static TeacherDaoImpl teacherDaoImpl = new TeacherDaoImpl();
     private static AttachmentDaoImpl attachmentDaoImpl = new AttachmentDaoImpl();
 
+    // 普通用户的专利管理页面
     @RequestMapping("/index")
     public String index(Map<String,Object> model, HttpSession session){
+        // 从session缓存中取到用户id
         Integer userId = (Integer) session.getAttribute("userId");
+        // 根据用户id获取到对应的老师记录
         Teacher teacher = teacherDaoImpl.findByUserId(userId);
+        // 查询出当前用户的所有专利记录
         List<Patent> patentList = patentDaoImpl.findAll(teacher.getId(), "");
+        // 放到model中推送到前端
         model.put("patentList", patentList);
 
+        // 访问index.jsp前端页面
         return "patent/index";
     }
 
+    // 管理员专利管理页面
     @RequestMapping("/admin_index")
     public String admin_index(Map<String,Object> model, HttpSession session){
+        // 查看所有用户的专利信息
         List<Patent> patentList = patentDaoImpl.adminFindAll("");
         model.put("patentList", patentList);
 
         return "patent/admin_index";
     }
 
+    // 新增专利页面
     @RequestMapping("/add")
     public String add(){
         return "patent/add";
     }
 
+    // 新增专利的具体操作
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public String create(Patent patent, HttpSession session, @RequestParam(value = "file") MultipartFile file) throws Exception{
         System.out.println("patent=" + patent);
 
+        // 上传附件到七牛云
         String url = null;
         if(!file.isEmpty()){
             url = QiniuUtil.uploadToQiNiuYun(file);
@@ -77,18 +88,22 @@ public class PatentController extends BaseController {
 
         System.out.println("last_result===" + url);
 
+        // 新建专利数据
         int patentId = patentDaoImpl.addPatent(patent);
         Integer userId = (Integer) session.getAttribute("userId");
         Teacher teacher = teacherDaoImpl.findByUserId(userId);
 
 //        int attachmentGroupId = AttachmentGroupService.getOrSetValue(patentId, "Patent", userId);
 
+        // 如果成功上传，则在数据库创建attachment对象，为了之后在页面显示和下载
         if(url != null) {
             attachmentService.setValue(file, url, userId, patentId, "Patent");
         }
 
+        // 创建中间表teacher_achie
         teacherAchieService.setValue(patentId, teacher, "Patent", "research", "submit");
 
+        // 判断用户角色，如果是管理员，则跳转到管理员的列表页，如果是普通用户，就跳转到普通用户的列表页
         String userType = (String) session.getAttribute("userType");
 
         if("admin".equals(userType)){
@@ -98,11 +113,16 @@ public class PatentController extends BaseController {
         }
     }
 
+    // 查看详情页面
     @RequestMapping("/show")
     public String show(Map<String,Object> model, HttpServletRequest request){
+        // 获取到专利id
         String patentId = request.getParameter("patentId");
+        // 根据专利id去数据库中查看专利数据
         Patent patent = patentDaoImpl.findById(Integer.parseInt(patentId));
+        // 根据专利id去数据库中查看上传的附件
         Attachment attachment = attachmentDaoImpl.findByOwnerIdAndOwnerType(patent.getId(), "Patent");
+        // 推送到前端显示
         model.put("patent", patent);
         System.out.println("patent=" + patent);
         System.out.println("file=" + attachment);
@@ -111,19 +131,25 @@ public class PatentController extends BaseController {
         return "patent/show";
     }
 
+    // 编辑专利页面
     @RequestMapping(value = "/edit", method = RequestMethod.GET)
     public String edit(HttpServletRequest request, Map<String,Object> model){
+        // 获取到专利id
         String patentId = request.getParameter("patentId");
+        // 根据id去数据库中得到专利信息
         Patent patent = patentDaoImpl.findById(Integer.parseInt(patentId));
         model.put("patent", patent);
+        // 根据专利id去数据库中查看上传的附件
         Attachment attachment = attachmentDaoImpl.findByOwnerIdAndOwnerType(patent.getId(), "Patent");
         model.put("attachment", attachment);
         return "patent/edit";
     }
 
+    // update是编辑专利的处理过程
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public String update(@ModelAttribute("patent") Patent patent, HttpSession session, @RequestParam(value = "file") MultipartFile file) throws Exception{
         System.out.println("file1=" + file);
+        // 上传新的附件
         String url = null;
         if(!file.isEmpty()) {
             url = QiniuUtil.uploadToQiNiuYun(file);
@@ -134,10 +160,12 @@ public class PatentController extends BaseController {
         System.out.println("patent=" + patent);
         System.out.println("file=" + file);
 
+        // 修改专利
         patentDaoImpl.editPatent(patent);
 
         System.out.println("patentId=" + patent.getId());
 
+        // 如果有新的附件那么就要把之前的attachment删除并生成新的attachment对象
         if(url != null) {
             Attachment attachment = attachmentDaoImpl.findByOwnerIdAndOwnerType(patent.getId(), "Patent");
 
@@ -152,6 +180,7 @@ public class PatentController extends BaseController {
 
         }
 
+        // 根据用户角色跳转到不同的列表页
         String userType = (String) session.getAttribute("userType");
 
         if("admin".equals(userType)){
@@ -161,6 +190,7 @@ public class PatentController extends BaseController {
         }
     }
 
+    // 查询按钮对应的方法
     @RequestMapping(value = "/search", method = RequestMethod.POST)
     public String search(HttpServletRequest request, Map<String,Object> model, HttpSession session) throws Exception {
 
@@ -168,12 +198,14 @@ public class PatentController extends BaseController {
 
             System.out.println("request===>" + request);
 
+            // 根据前端传的参数生成查询语句
             String querySql = QueryUtil.generateQuerySql(request);
             System.out.println("querysql====>" + querySql);
 
             Integer userId = (Integer) session.getAttribute("userId");
             Teacher teacher = teacherDaoImpl.findByUserId(userId);
 
+            // 根据查询语句去数据库中查找
             List<Patent> patentList = patentDaoImpl.findAll(teacher.getId(), querySql);
 
             System.out.println("patentList====>" + patentList);
@@ -191,6 +223,7 @@ public class PatentController extends BaseController {
 
     }
 
+    // 管理员的查询按钮
     @RequestMapping(value = "/admin_search", method = RequestMethod.POST)
     public String admin_search(HttpServletRequest request, Map<String,Object> model, HttpSession session) throws Exception {
 
@@ -198,9 +231,11 @@ public class PatentController extends BaseController {
 
             System.out.println("request===>" + request);
 
+            // 根据前端传的参数生成查询语句
             String querySql = QueryUtil.generateQuerySql(request);
             System.out.println("querysql====>" + querySql);
 
+            // 管理员的查询方法 可以查看所有用户数据
             List<Patent> patentList = patentDaoImpl.adminFindAll(querySql);
 
             System.out.println("patentList====>" + patentList);
--- src/main/java/achieve/util/DBUtil.java
+++ src/main/java/achieve/util/DBUtil.java
@@ -5,12 +5,16 @@ import java.sql.DriverManager;
 
 public class DBUtil {
 
+    // 数据库驱动
     private static String driverName = "com.mysql.jdbc.Driver" ;
+    // 数据库地址和名称以及编码方式
     private static String url = "jdbc:mysql://45.76.103.254:3306/achie_0417?characterEncoding=utf8&useSSL=false" ;
+    // 数据库账户
     private static String username = "root" ;
+    // 数据库密码
     private static String password = "dodocat1994" ;
 
-//    建立连接
+//    开始做操作前建立连接
     public static Connection getConnection()throws Exception{
         try {
             Class.forName(driverName) ;
@@ -26,7 +30,7 @@ public class DBUtil {
 
     }
 
-//    关闭数据库
+//    操作完后关闭数据库
     public static void close(Connection conn){
         if(conn != null){
             try {
@@ -37,7 +41,7 @@ public class DBUtil {
         }
     }
 
-//    回滚
+//    有错误要回滚
     public static void rollback(Connection conn){
         if(conn != null){
             try {
